fa067317fbb49bd4b2b0e313d36f419b890104a7
pawan999333 <pawansharma19feb@gmail.com>
2023-09-30 23:02:46 +0530

    components

---
+++ pawan/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import SubComponent from './components/SubComponent';
+
+
+function App() {
+  return (
+    // <div className="app-container">
+    //   <h3>First Component</h3>
+    //   <p>This is a p tag</p>
+    // </div>
+
+
+
+    // {/* <SubComponent></SubComponent> */}
+    <SubComponent />
+
+  );
+}
+
+export default App;
---
+++ pawan/src/components/SubComponent.js
@@ -0,0 +1,7 @@
+import './SubComponent.css';
+
+function SubComponent() {
+  return <h1 className="red">This is a SubComponent</h1>;
+}
+
+export default SubComponent;
